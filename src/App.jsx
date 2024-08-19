7dae9c15e14ba4fee95b9343a126e386a117a95d
madyanes <madyanes.dev@gmail.com>
2024-08-19 14:14:14 +0700

    Create reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useReducer } from 'react'
 import './styles.css'
 
 /*
@@ -24,40 +25,64 @@ const initialState = {
   isActive: false,
 }
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'openAccount':
+      return ''
+    case 'deposit':
+      return ''
+    case 'withdraw':
+      return ''
+    case 'requestLoan':
+      return ''
+    case 'payLoan':
+      return ''
+    case 'closeAccount':
+      return ''
+    default:
+      return new Error('Unknown account')
+  }
+}
+
 function App() {
+  const [{ balance, loan, isActive }, dispatch] = useReducer(
+    reducer,
+    initialState
+  )
+
   return (
     <div className='App'>
       <h1>useReducer Bank Account</h1>
-      <p>Balance: X</p>
-      <p>Loan: X</p>
+      <p>Balance: {balance}</p>
+      <p>Loan: {loan}</p>
 
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={isActive}>
           Open account
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Deposit 150
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Withdraw 50
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Request a loan of 5000
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Pay loan
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Close account
         </button>
       </p>
